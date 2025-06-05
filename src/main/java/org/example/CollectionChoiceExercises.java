4cdc427978609ec76d9ca9e291a9da622d615a33
Shravan Samudrala <shravans5811@gmail.com>
2025-06-05 10:02:28 -0500

    Added example exercise and improved instructions.

--- src/main/java/org/example/CollectionChoiceExercises.java
+++ src/main/java/org/example/CollectionChoiceExercises.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 // For these exercises, you need to declare a single variable
 // in each function that uses the appropriate collection type
 // (Array, ArrayList, HashSet, HashMap)
@@ -13,12 +17,18 @@ public class CollectionChoiceExercises {
         // of three strings that are known ahead of time to be "Red", "Green" and "Blue"
         // and will not change during program execution.
 
-        String[] colors;
+        String[] colors = {"Red", "Green", "Blue"};
+        for (String color : colors) System.out.println(color);
     }
 
     public void MakeStudentCollection() {
         // Create an appropriate collection variable for holding a list of student names
         // that will be added to and removed from during program execution.
+        ArrayList<String> studentNames = new ArrayList<>();
+        studentNames.add("John");
+        studentNames.add("Sam");
+        System.out.println(studentNames);
+//        return studentNames;
     }
 
     public void MakeLoggedInUsersCollection() {
@@ -26,11 +36,19 @@ public class CollectionChoiceExercises {
         // unique logged in usernames where adding a username that's already
         // present is ignored and checking if a user is in the collection can
         // be done very quickly.
+        HashSet<String> userNames = new HashSet<>();
+        userNames.add("Jhasbd1232");
+        userNames.add("Usdjbcj1234");
+        System.out.println(userNames);
     }
 
     public void MakeEmployeeAccessCodesCollection() {
         // Create an appropriate collection variable for holding a set of
         // key-value pairs where they key is an employee name (String) and the
         // value is an employee access code (String.)
+
+        HashMap<String, String> employeeInfo = new HashMap<>();
+            employeeInfo.put("Sam", "HJSDCV");
+        System.out.println("EmployeeInfo" + employeeInfo);
     }
 }
